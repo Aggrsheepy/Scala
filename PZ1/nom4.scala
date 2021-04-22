b876bea34689e690f5545b1a492aee645a803e59
Aggrsheepy <39929477+Aggrsheepy@users.noreply.github.com>
2021-04-22 12:43:45 +0300

    Create nom4.scala

---
+++ PZ1/nom4.scala
@@ -0,0 +1,11 @@
+object nom4 extends App {
+   
+  def length[A](ls: List[A]): Int = ls.length
+  
+  def main (args: Array[String]) {
+      print (length(List(1, 1, 2, 3, 5, 8)))
+    
+    }
+}
+
+// к-во элементов списка
