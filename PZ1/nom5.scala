8d5c800e79056bf660e82b8a84cfec71bd69dd41
Aggrsheepy <39929477+Aggrsheepy@users.noreply.github.com>
2021-04-22 12:47:11 +0300

    Create nom5.scala

---
+++ PZ1/nom5.scala
@@ -0,0 +1,10 @@
+object nom5 extends App {
+
+    def reverse[A](ls: List[A]): List[A] = ls.reverse
+
+    def main (args: Array[String]) {
+      print (reverse(List(1, 1, 2, 3, 5, 8)))
+      
+    }
+  }
+// разворачивает список 
