0ba631f542fc27aa900e95378fa93b18b9a29759
Aggrsheepy <39929477+Aggrsheepy@users.noreply.github.com>
2021-04-22 12:53:23 +0300

    Create nom6.scala

---
+++ PZ1/nom6.scala
@@ -0,0 +1,11 @@
+object nom6 extends App {
+  
+  def isPalindrome[A](ls:List[A]): Boolean = ls == ls.reverse
+  
+  def main (args: Array[String]) {
+    print(isPalindrome(List((1, 2, 3, 2, 1))))
+    
+  }
+}
+
+//Определяет, является ли список палиндромом (одинаково читается и слева, и справа)
